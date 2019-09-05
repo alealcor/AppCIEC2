37b82df3a26d31174402c598ba926aeeaf06ead3
unknown <jparrales15@gmail.com>
2019-09-05 00:04:05 -0500

    Agregando marcadores y colores de poligonos

--- app/src/main/java/com/example/jonathan/appciec/Fragments/MapasActivity.java
+++ app/src/main/java/com/example/jonathan/appciec/Fragments/MapasActivity.java
@@ -25,8 +25,9 @@ import com.google.maps.android.data.geojson.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import com.google.gson.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapasActivity extends Fragment implements GoogleMap.OnMyLocationButtonClickListener,
@@ -93,30 +94,68 @@ public class MapasActivity extends Fragment implements GoogleMap.OnMyLocationBut
                     //Log.d("Stilo", feature.getProperty("nom_barrio").toString());
                     GeoJsonPolygonStyle polygonStyle = new GeoJsonPolygonStyle();
                     if(polygonStyle != null){
-                        //Log.d("Menj", "entro");
-                        //polygonStyle.setStrokeColor(getContext().getColor(R.color.colorPrimary));
-                        //polygonStyle.setFillColor(getContext().getColor(R.color.colorPrimary));
-                        //feature.setPolygonStyle(polygonStyle);
-                        Geometry point = feature.getGeometry();
-                        String example = point.getGeometryObject().toString();
-                        JSONObject obj = null;
-                        String tmp = "vacios";
-                        try {
-                            obj = new JSONObject(example);
-                           tmp = obj.keys().toString();
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                        double valor = Double.parseDouble(feature.getProperty("pobbarr"));
+                        if(valor < 18){
+                            polygonStyle.setStrokeColor(getContext().getColor(R.color.colorMap8));
+                            polygonStyle.setFillColor(getContext().getColor(R.color.colorMap8));
+                            feature.setPolygonStyle(polygonStyle);
+                        }else if (valor >= 18 && valor < 224){
+                            polygonStyle.setStrokeColor(getContext().getColor(R.color.colorMap7));
+                            polygonStyle.setFillColor(getContext().getColor(R.color.colorMap7));
+                            feature.setPolygonStyle(polygonStyle);
+                        }else if (valor >= 224 && valor < 411){
+                            polygonStyle.setStrokeColor(getContext().getColor(R.color.colorMap6));
+                            polygonStyle.setFillColor(getContext().getColor(R.color.colorMap6));
+                            feature.setPolygonStyle(polygonStyle);
+                        }else if (valor >= 411 && valor < 514){
+                            polygonStyle.setStrokeColor(getContext().getColor(R.color.colorMap5));
+                            polygonStyle.setFillColor(getContext().getColor(R.color.colorMap5));
+                            feature.setPolygonStyle(polygonStyle);
+                        }else if (valor >= 514 && valor < 685){
+                            polygonStyle.setStrokeColor(getContext().getColor(R.color.colorMap4));
+                            polygonStyle.setFillColor(getContext().getColor(R.color.colorMap4));
+                            feature.setPolygonStyle(polygonStyle);
+                        }else if (valor >= 685 && valor < 1337){
+                            polygonStyle.setStrokeColor(getContext().getColor(R.color.colorMap3));
+                            polygonStyle.setFillColor(getContext().getColor(R.color.colorMap3));
+                            feature.setPolygonStyle(polygonStyle);
+                        }else if (valor >= 1337 && valor < 2873){
+                            polygonStyle.setStrokeColor(getContext().getColor(R.color.colorMap2));
+                            polygonStyle.setFillColor(getContext().getColor(R.color.colorMap2));
+                            feature.setPolygonStyle(polygonStyle);
+                        }else{
+                            polygonStyle.setStrokeColor(getContext().getColor(R.color.colorMap1));
+                            polygonStyle.setFillColor(getContext().getColor(R.color.colorMap1));
+                            feature.setPolygonStyle(polygonStyle);
                         }
-                        Log.d("Lista", tmp );
 
-                        //String geometryType = punto.getCenter().toString();
-                        /*if(punto != null){
-                            LatLng puntero = punto.get(1);
+                        List<Geometry> geometryList = (List<Geometry>) feature.getGeometry().getGeometryObject();
+                        ArrayList<GeoJsonPolygon> geoJsonPolygon = new ArrayList<GeoJsonPolygon>();
+                        for (Geometry geometry : geometryList) {
+                            GeoJsonPolygon polygon = (GeoJsonPolygon) geometry;
+                            geoJsonPolygon.add(polygon);
+                        }
+                        ArrayList<LatLng> puntos = new ArrayList<LatLng>();
+                        for(GeoJsonPolygon polygon : geoJsonPolygon){
+                             puntos =  polygon.getOuterBoundaryCoordinates();
+                        }
+                        double[] centroid = { 0.0, 0.0 };
+                        for (int i = 0; i < puntos.size(); i++) {
+                            centroid[0] += puntos.get(i).latitude;
+                            centroid[1] += puntos.get(i).longitude;
+                        }
+                        int totalPoints = puntos.size();
+                        centroid[0] = centroid[0] / totalPoints;
+                        centroid[1] = centroid[1] / totalPoints;
+
+                        LatLng centroide = new LatLng(centroid[0], centroid[1]);
+
+                        if(centroide != null){
                             mMap.addMarker(new MarkerOptions()
-                                    .position(puntero)
+                                    .position(centroide)
                                     .title(feature.getProperty("nom_barrio"))
-                                    .snippet(""));
-                        }*/
+                                    .snippet("Población: "+ feature.getProperty("pobbarr")+ " personas"));
+                        }
                     }
                     Log.d("Menj", "No entro");
                 }
